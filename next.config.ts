8eaf86d1f8d8a4b822ca2f8778de411f0d996ed1
Tizar Fitrah <Tizar Fitrah@LAPTOP-FBH3SH43.(none)>
2026-05-25 14:07:32 +0800

    Mengabaikan build errors di next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Mengabaikan error linting dan typescript saat proses deploy agar website tetap bisa online
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  devIndicators: false,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
